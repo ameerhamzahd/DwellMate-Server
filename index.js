6a6fe657237ab55903a569d56c74de91636f4270
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-05-24 02:25:57 +0600

    DwellMate-server SavingTheCounter-Functionality done.

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@dwellmate.lzqu57f.mongodb.net/?retryWrites=true&w=majority&appName=DwellMate`;
 
+app.use(cors());
+app.use(express.json());
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -18,7 +21,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const propertyCollection = client.db("DwellMateDB").collection("properties");
 
@@ -71,13 +74,32 @@ async function run() {
     })
 
     // FOR LIMIT OPERATOR
-    app.get("/properties/listings/:availability", async(request, response) => {
+    app.get("/properties/listings/:availability", async (request, response) => {
       const available = request.params.availability;
       const query = { availability: "Available" };
       const result = await propertyCollection.find(query).limit(6).toArray();
 
       response.send(result);
     });
+
+    // SAVING THE COUNTER OF LIKE BUTTON
+    app.put("/properties/likes/:id", async (request, response) => {
+      const id = request.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $inc: { likes: 1 }
+      };
+      const options = {
+        returnDocument: "after"
+      };
+      const result = await propertyCollection.findOneAndUpdate(
+        filter,
+        updateDoc,
+        options
+      );
+
+      response.send(result.value);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -88,10 +110,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-
-app.use(cors());
-app.use(express.json());
 
 app.get("/", (request, response) => {
   response.send("DwellMate is running...")
